a7d3aa117b7a6909405553feecdb3ec4b81a2139
goutham32 <70678979+goutham32@users.noreply.github.com>
2020-09-14 12:39:18 +0530

    Update and rename userClassTest.php to UserTest.php

---
+++ tests/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function test_getUser()
+    {
+        require 'config/User.php';
+        $value = ['first_name'=>"Thrikkalangode",'last_name'=>"32",'user_name'=>"goutham",'password'=>"123456",'dob'=>"2018-03-12"];
+        $dataBase = $this->getMockBuilder('Database')->getMock();
+        $dataBase->method('run')->willReturn($value);
+        $expectedResult = [
+           'first_name' => 'Thrikkalangode',
+           'last_name'  => '32',
+           'user_name'  => 'goutham',
+           'password'   => '123456',
+           'dob'        => '2018-03-12'
+          ];
+        $user = new USER($dataBase);
+        $this->assertEquals($expectedResult, $user->getUser());
+       
+    }
+}
+
+
+
