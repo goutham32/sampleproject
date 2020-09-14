02f10ea92aa0035f6584f7511eb609e13e60df22
goutham32 <70678979+goutham32@users.noreply.github.com>
2020-09-14 16:42:23 +0530

    Update UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,12 +1,13 @@
 <?php
 
+use \Mockery;
+use \User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
     public function test_getUser()
     {
-        require 'config/User.php';
         $value = ['first_name' => "Thrikkalangode", 'last_name' => "32", 'user_name' => "goutham", 'password' => "123456", 'dob' => "2018-03-12"];
         $dataBase = Mockery::mock('Database');
         $dataBase->shouldReceive('run')
@@ -20,8 +21,9 @@ class UserTest extends TestCase
             'password'   => '123456',
             'dob'        => '2018-03-12'
         ];
-        $user = new USER($dataBase);
+        $user = new User($dataBase);
         $actualResult = $user->getUser();
         $this->assertEquals($expectedResult, $actualResult);
     }
 }
+
