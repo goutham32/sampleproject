7ec647855680678c53506e917aa9c6f6d51af2e8
goutham32 <70678979+goutham32@users.noreply.github.com>
2020-09-14 14:04:14 +0530

    Update UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,4 +1,5 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
@@ -6,21 +7,17 @@ class UserTest extends TestCase
     public function test_getUser()
     {
         require 'config/User.php';
-        $value = ['first_name'=>"Thrikkalangode",'last_name'=>"32",'user_name'=>"goutham",'password'=>"123456",'dob'=>"2018-03-12"];
+        $value = ['first_name' => "Thrikkalangode", 'last_name' => "32", 'user_name' => "goutham", 'password' => "123456", 'dob' => "2018-03-12"];
         $dataBase = $this->getMockBuilder('Database')->getMock();
         $dataBase->method('run')->willReturn($value);
         $expectedResult = [
-           'first_name' => 'Thrikkalangode',
-           'last_name'  => '32',
-           'user_name'  => 'goutham',
-           'password'   => '123456',
-           'dob'        => '2018-03-12'
-          ];
+            'first_name' => 'Thrikkalangode',
+            'last_name'  => '32',
+            'user_name'  => 'goutham',
+            'password'   => '123456',
+            'dob'        => '2018-03-12'
+        ];
         $user = new USER($dataBase);
         $this->assertEquals($expectedResult, $user->getUser());
-       
     }
 }
-
-
-
